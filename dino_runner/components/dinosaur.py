5285e807ede409b49780605701af09bf6db63eff
Jonatas <jonatasolmartins@outlook.com>
2022-12-13 21:18:35 -0300

    Adding the duck function at Dinosaur class.

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,11 +1,12 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 RUN_IMG = [RUNNING[0], RUNNING[1]]
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
+Y_POS_DUCK = 340
 
 class Dinosaur:
     def __init__(self):
@@ -17,6 +18,7 @@ class Dinosaur:
         self.running: bool = False
         self.jump: bool = False
         self.jump_vel = JUMP_VEL
+        self.dino_duck = False
 
     def update(self, user_input):
 
@@ -24,12 +26,20 @@ class Dinosaur:
              self.run()
         elif self.jump:
             self.dino_jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.jump:
             self.running = False
+            self.dino_duck = False
             self.jump = True
-        elif not self.jump:
+        elif user_input[pygame.K_DOWN] and not self.jump:
+            self.running = False
             self.jump = False
+            self.dino_duck = True
+        elif not self.jump and not self.dino_duck:
+            self.jump = False
+            self.dino_duck = False
             self.running = True
 
         if self.step_index >= 9:
@@ -55,14 +65,23 @@ class Dinosaur:
         self.jump_vel -= 0.8
 
         if self.jump_vel < -JUMP_VEL:
-            # self.dino_rect.y += self.jump_vel * 4
-            # self.jump_vel += 1
             self.dino_rect.y = Y_POS
             self.jump = False
             self.jump_vel = JUMP_VEL
+             # self.dino_rect.y += self.jump_vel * 4
+            # self.jump_vel += 1
           
     def duck(self):
-        pass
+        self.image = DUCKING[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
+        self.step_index += 1
+        self.dino_duck = False
+        #O operador // divide e o resultado é sempre um numero inteiro, o mais proximo da divisão
+        # O operador / divide e o resultado é um numero com ponto flutuante, o mais preciso
+        # for i in range(0, 10):
+        #    print(i // 5)
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
